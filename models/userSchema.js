b603aa1398833053857cc22ea543821c7e110e0f
keerthanacm <cmkeerthana2001@gmail.com>
2024-07-20 22:18:26 +0530

    user schema

---
+++ models/userSchema.js
@@ -0,0 +1,31 @@
+const mongoose =require("mongoose")
+const userSchema = new mongoose.Schema({
+  fullName:{
+    type:String,
+    required: true
+  },
+  phone:{
+    type:String,
+    required: true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  gender:{
+    type:String,
+    required:true,
+    enum:["male","female"]
+  },
+  role:{
+    type:String,
+    required:true
+  
+  }
+})
+
+module.exports = mongoose.model("User", userSchema);
