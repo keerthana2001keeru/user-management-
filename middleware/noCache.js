d80b061896dd5f040b841c35318e49348a30e1c0
keerthanacm <cmkeerthana2001@gmail.com>
2024-07-24 00:29:53 +0530

    cache control for lget

---
+++ middleware/noCache.js
@@ -0,0 +1,6 @@
+const noCacheMiddleware= function(req, res, next) {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    next();
+}
+
+module.exports = noCacheMiddleware;
